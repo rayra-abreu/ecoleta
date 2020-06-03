58eaed29662b910e73717abcc709f327e1acc5ea
Rayra <rayramariana_live@hotmail.com>
2020-06-03 13:52:33 -0300

    Criando o projeto ReactJS

---
+++ web/src/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+//FC (Function Component) um componente escrito em formato de função
+//FC é um generic dentro do typescript
+interface HeaderProps{
+  title: string
+  //title?: string
+}
+
+const Header: React.FC<HeaderProps>=(props)=>{
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  )
+}
+
+export default Header
