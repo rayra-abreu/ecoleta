fedd579d2e2a58b68566b600a94d3cfb0566ec9a
Rayra <rayramariana_live@hotmail.com>
2020-06-02 22:43:01 -0300

    Desacoplando o código da aplicação

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,56 +1,18 @@
 import express from 'express'
-import knex from './database/connection'
 
+import PointsController from './controllers/pointsController'
+import ItemsController from './controllers/itemsController'
+
+/*Padrão da comunidade: index(listagem), show(exibir um único registro daquele,
+create(store), update, delete(ou destroy))*/
 const routes=express.Router()
+const pointsController=new PointsController()
+const itemsController=new ItemsController()
 
-routes.get('/items', async(request, response)=>{
-  const items=await knex('items').select('*')
-  const serializedItems=items.map(item=>{
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    }
-  })
-  return response.json(serializedItems)
-})
+routes.get('/items', itemsController.index)
+routes.post('/points', pointsController.create)
 
-routes.post('/points', async(request, response)=>{
-  const{
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  }=request.body
-
-  const trx=await knex.transaction()
-
-  const insertedIds=await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  })
-
-  const point_id=insertedIds[0]
-
-  const pointItems=items.map((item_id: number)=>{
-    return{
-      item_id,
-      point_id,
-    }
-  })
-
-  await trx('point_items').insert(pointItems)
-
-  return response.json({success: true})
-})
 export default routes
+
+//Service Pattern
+//Repository Pattern(Data Mapper)
