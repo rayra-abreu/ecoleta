451c42df075fc7b410161f92f55968f7154ee272
Rayra <rayramariana_live@hotmail.com>
2020-06-07 00:33:29 -0300

    Validando entrada de dados na criação de pontos de coleta

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import routes from './routes'
+import {errors} from 'celebrate'
 
 const app=express()
 
@@ -60,4 +61,6 @@ app.post('/users', (request, response)=>{
 
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(errors())
+
 app.listen(3333)
