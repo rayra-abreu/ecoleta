197e337ed3d742875def8392b1dfdbb1cf2ae5f9
Rayra <rayramariana_live@hotmail.com>
2020-09-28 00:09:40 -0300

    Separação de estilos

---
+++ mobile/src/pages/Home/styles.ts
@@ -0,0 +1,86 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  label: {
+    marginVertical: 10,
+    color: '#6C6C80',
+    fontSize: 16,
+    fontFamily: 'Roboto_500Medium',
+  },
+
+  select: {
+    color: '#6C6C80',
+  },
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 14,
+    borderBottomWidth: 1.6,
+    borderStyle: 'solid',
+    justifyContent: 'center',
+    borderBottomColor: '#322153',
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius:10,
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  }
+})
+
+export default styles
